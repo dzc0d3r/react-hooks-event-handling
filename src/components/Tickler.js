4ca8a3e7cf62373400f889482de8251e18adc892
dzc0d3r <m4dcod3r@protonmail.com>
2023-09-11 15:24:11 +0100

    Finished assignement :tada:

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from "react"
 
-function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
+function Tickler () {
+  function tickle () {
+    console.log("Teehee!")
   }
+  /** the event handler in react is SyntheticEvent  */
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>
 }
 
-export default Tickler;
+export default Tickler
